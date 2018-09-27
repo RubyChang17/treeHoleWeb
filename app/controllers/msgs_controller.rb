4f8054aabf31aa40833c5355a82cf8d06b34c3df
rubychang17 <a10223021@gmail.com>
2018-09-27 13:22:41 +0800

    controller & model

---
+++ app/controllers/msgs_controller.rb
@@ -0,0 +1,13 @@
+class MsgsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+	root 'msgs#index'
+
+  get 'msgs/index'
+  get 'msgs/show'
+  get 'msgs/new'
+  get 'msgs/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
